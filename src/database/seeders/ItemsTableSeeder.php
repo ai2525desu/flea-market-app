a0fec2e9351adbfed632eefee5f55432e8dcb796
ai2525desu <umi08062525@gmail.com>
2025-07-31 23:18:41 +0900

    ダミーデータ作成完了

--- src/database/seeders/ItemsTableSeeder.php
+++ src/database/seeders/ItemsTableSeeder.php
@@ -18,6 +18,7 @@ class ItemsTableSeeder extends Seeder
 
         // 商品１：腕時計
         $itemId = DB::table('items')->insertGetId([
+            'user_id' => '1',
             'item_name' => '腕時計',
             'item_image' => 'items/Wristwatch.jpg',
             'brand' => null,
@@ -30,18 +31,147 @@ class ItemsTableSeeder extends Seeder
             ['item_id' => $itemId, 'category_id' => $categories['メンズ']]
         ]);
 
-        // 商品2：
+        // 商品2：HDD
         $itemId = DB::table('items')->insertGetId([
-            'item_name' => '',
-            'item_image' => '',
-            'brand' => '',
-            'price' => '',
-            'description' => '',
-            'condition' => '',
+            'user_id' => '1',
+            'item_name' => 'HDD',
+            'item_image' => 'items/HDD.jpg',
+            'brand' => null,
+            'price' => 5000,
+            'description' => '高速で信頼性の高いハードディスク',
+            'condition' => '目立った傷や汚れなし',
         ]);
         DB::table('category_item')->insert([
-            ['item_id' => $itemId, 'category_id' => $categories['']],
-            ['item_id' => $itemId, 'category_id' => $categories['']]
+            ['item_id' => $itemId, 'category_id' => $categories['家電']],
+            ['item_id' => $itemId, 'category_id' => $categories['ゲーム']]
+        ]);
+
+        // 商品3：玉ねぎ3束
+        $itemId = DB::table('items')->insertGetId([
+            'user_id' => '1',
+            'item_name' => '玉ねぎ3束',
+            'item_image' => 'items/Onion.jpg',
+            'brand' => null,
+            'price' => 300,
+            'description' => '新鮮な玉ねぎ3束のセット',
+            'condition' => 'やや傷や汚れあり',
+        ]);
+        DB::table('category_item')->insert([
+            ['item_id' => $itemId, 'category_id' => $categories['キッチン']]
+        ]);
+
+
+        // 商品4：革靴
+        $itemId = DB::table('items')->insertGetId([
+            'user_id' => '1',
+            'item_name' => '革靴',
+            'item_image' => 'items/LeatherShoes.jpg',
+            'brand' => null,
+            'price' => 4000,
+            'description' => 'クラシックなデザインの革靴',
+            'condition' => '状態が悪い',
+        ]);
+        DB::table('category_item')->insert([
+            ['item_id' => $itemId, 'category_id' => $categories['ファッション']],
+            ['item_id' => $itemId, 'category_id' => $categories['メンズ']]
+        ]);
+
+
+        // 商品5：ノートPC
+        $itemId = DB::table('items')->insertGetId([
+            'user_id' => '1',
+            'item_name' => 'ノートPC',
+            'item_image' => 'items/Laptop.jpg',
+            'brand' => null,
+            'price' => 45000,
+            'description' => '高性能なノートパソコン',
+            'condition' => '良好',
+        ]);
+        DB::table('category_item')->insert([
+            ['item_id' => $itemId, 'category_id' => $categories['家電']]
+        ]);
+
+
+
+        // 商品6：マイク
+        $itemId = DB::table('items')->insertGetId([
+            'user_id' => '1',
+            'item_name' => 'マイク',
+            'item_image' => 'items/Microphone.jpg',
+            'brand' => null,
+            'price' => 8000,
+            'description' => '高音質のレコーディング用マイク',
+            'condition' => '目立った傷や汚れなし',
+        ]);
+        DB::table('category_item')->insert([
+            ['item_id' => $itemId, 'category_id' => $categories['おもちゃ']],
+            ['item_id' => $itemId, 'category_id' => $categories['ベビー・キッズ']]
+        ]);
+
+
+        // 商品7：ショルダーバッグ
+        $itemId = DB::table('items')->insertGetId([
+            'user_id' => '1',
+            'item_name' => 'ショルダーバッグ',
+            'item_image' => 'items/ShoulderBag.jpg',
+            'brand' => null,
+            'price' => 3500,
+            'description' => 'おしゃれなショルダーバッグ',
+            'condition' => 'やや傷や汚れあり',
+        ]);
+        DB::table('category_item')->insert([
+            ['item_id' => $itemId, 'category_id' => $categories['ファッション']],
+            ['item_id' => $itemId, 'category_id' => $categories['レディース']]
+        ]);
+
+
+        // 商品8：タンブラー
+        $itemId = DB::table('items')->insertGetId([
+            'user_id' => '1',
+            'item_name' => 'タンブラー',
+            'item_image' => 'items/Tumbler.jpg',
+            'brand' => null,
+            'price' => 500,
+            'description' => '使いやすいタンブラー',
+            'condition' => '状態が悪い',
+        ]);
+        DB::table('category_item')->insert([
+            ['item_id' => $itemId, 'category_id' => $categories['キッチン']],
+            ['item_id' => $itemId, 'category_id' => $categories['インテリア']]
+        ]);
+
+
+
+        // 商品9：コーヒーミル
+        $itemId = DB::table('items')->insertGetId([
+            'user_id' => '1',
+            'item_name' => 'コーヒーミル',
+            'item_image' => 'items/CoffeeMill.jpg',
+            'brand' => null,
+            'price' => 4000,
+            'description' => '手動のコーヒーミル',
+            'condition' => '良好',
+        ]);
+        DB::table('category_item')->insert([
+            ['item_id' => $itemId, 'category_id' => $categories['キッチン']],
+            ['item_id' => $itemId, 'category_id' => $categories['インテリア']]
+        ]);
+
+
+        // 商品10：メイクセット
+        $itemId = DB::table('items')->insertGetId([
+            'user_id' => '1',
+            'item_name' => 'メイクセット',
+            'item_image' => 'items/MakeupSet.jpg',
+            'brand' => null,
+            'price' => 2500,
+            'description' => '便利なメイクアップセット',
+            'condition' => '目立った傷や汚れなし',
+        ]);
+        DB::table('category_item')->insert([
+            ['item_id' => $itemId, 'category_id' => $categories['ファッション']],
+            ['item_id' => $itemId, 'category_id' => $categories['レディース']],
+            ['item_id' => $itemId, 'category_id' => $categories['コスメ']]
         ]);
     }
 }
---
+++ src/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'テスト太郎',
+            'email' => 'test1234@example.co.jp',
+            'password' => bcrypt('password'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
