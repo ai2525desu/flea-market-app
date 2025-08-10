d2bf70a2041d9716e50ab49866acb445bfe4a018
ai2525desu <umi08062525@gmail.com>
2025-08-10 21:41:23 +0900

    ログイン画面ビュー完成、レスポンシブ対応済み

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -5,5 +5,55 @@
 @endsection
 
 @section('content')
-ログイン画面
+<div class="login-form__content">
+    <div class="login-form__form-wrap">
+        <h2 class="login-form__heading">
+            ログイン
+        </h2>
+        <form class="login-form__form" action="{{ route('auth.login') }}" method="post" novalidate>
+            @csrf
+            <div class="login-form__group">
+                <div class="login-form__group">
+                    <div class="login-form__group--title">
+                        <label for="email" class="login-form__group--label">
+                            メールアドレス
+                        </label>
+                    </div>
+                    <div class="login-form__group--content">
+                        <input id="email" type="email" name="email" value="{{ old('email') }}">
+                    </div>
+                    <div class="login-form__group--error">
+                        @error('email')
+                        {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+                <div class="login-form__group">
+                    <div class="login-form__group--title">
+                        <label for="password" class="login-form__group--label">
+                            パスワード
+                        </label>
+                    </div>
+                    <div class="login-form__group--content">
+                        <input id="password" type="password" name="password">
+                    </div>
+                    <div class="login-form__group--error">
+                        @error('password')
+                        {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+                <div class="login-form__button">
+                    <button type="submit" class="login-form__button--submit">
+                        ログインする
+                    </button>
+                </div>
+        </form>
+    </div>
+    <div class="login-form__content--screen-transition">
+        <a class="screen-transition__register" href="{{ route('auth.register') }}">
+            会員登録はこちら
+        </a>
+    </div>
+</div>
 @endsection
