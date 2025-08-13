726424f4bb1539fe4691d90ca990591f59ee541d
ai2525desu <umi08062525@gmail.com>
2025-08-14 00:36:33 +0900

    プロフィール設定画面のビュー作成中

--- src/resources/views/profiles/edit.blade.php
+++ src/resources/views/profiles/edit.blade.php
@@ -15,11 +15,20 @@
             @csrf
             <div class="profile-edit-form__group">
                 <div class="profile-edit-form__group--content">
-                    <!-- imageの登録処理 -->
-                    <input type="file" name="image">
+                    <div class="profile-image__wrap">
+                        <div class="profile-image__content">
+                            <!-- この部分、後で編集時に変更必須もぎたてのdetail参考に -->
+                            <img id="profile-image" src="#" class="profile-image" alt="プロフィール画像">
+                            <input id="image" type="file" name="image" class="profile-image__input" accept="image/">
+                        </div>
+                        <div class="profile-image__label">
+                            <label for="image" class="profile-image__button">画像を選択する</label>
+                            <span id="selected-filename" class="filename-display"></span>
+                        </div>
+                    </div>
                 </div>
                 <div class="profile-edit-form__group--error">
-                    @error('name')
+                    @error('image')
                     {{ $message }}
                     @enderror
                 </div>
@@ -39,6 +48,53 @@
                     @enderror
                 </div>
             </div>
+            <!-- 郵便番号 -->
+            <div class="profile-edit-form__group">
+                <div class="profile-edit-form__group--title">
+                    <label for="post_code" class="profile-edit-form__group--label">
+                        郵便番号
+                    </label>
+                </div>
+                <div class="profile-edit-form__group--content">
+                    <input id="post_code" type="text" name="post_code" value="{{ old('post_code') }}">
+                </div>
+                <div class="profile-edit-form__group--error">
+                    @error('post_code')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+            <!-- 住所 -->
+            <div class="profile-edit-form__group">
+                <div class="profile-edit-form__group--title">
+                    <label for="address" class="profile-edit-form__group--label">
+                        住所
+                    </label>
+                </div>
+                <div class="profile-edit-form__group--content">
+                    <input id="address" type="text" name="address" value="{{ old('address') }}">
+                </div>
+                <div class="profile-edit-form__group--error">
+                    @error('address')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+            <div class="profile-edit-form__group">
+                <div class="profile-edit-form__group--title">
+                    <label for="building" class="profile-edit-form__group--label">
+                        建物名
+                    </label>
+                </div>
+                <div class="profile-edit-form__group--content">
+                    <input id="building" type="text" name="building" value="{{ old('building') }}">
+                </div>
+                <div class="profile-edit-form__group--error">
+                    @error('building')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
             <div class="profile-edit-form__button">
                 <button type="submit" class="profile-edit-form__button--submit">
                     更新する
@@ -47,4 +103,21 @@
         </form>
     </div>
 </div>
+
+<script>
+    document.getElementById('image').addEventListener('change', function(e) {
+        const file = e.target.files[0];
+        if (!file) return;
+
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const preview = document.getElementById('profile-image');
+            preview.src = e.target.result;
+            preview.style.display = 'block';
+        }
+        reader.readAsDataURL(file);
+
+        document.getElementById('selected-filename').textContent = file.name;
+    });
+</script>
 @endsection
