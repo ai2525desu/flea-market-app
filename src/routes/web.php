70a89d6afc2b44a51d7919d037c6262fb60e4ec7
ai2525desu <umi08062525@gmail.com>
2025-08-23 21:10:30 +0900

    共通レイアウト調整完了

--- src/routes/web.php
+++ src/routes/web.php
@@ -26,7 +26,6 @@ Route::get('/login', [AuthController::class, 'login'])->name('auth.login');
 Route::post('/login', [AuthController::class, 'authenticate']);
 
 // 認証ミドルウェアの必要な部分は下記に記述。そうすると自動的に未認証の場合はログイン画面に遷移する
-// 現在ブラウザ表示の確認のためにミドルウェアOFF
 Route::middleware('auth')->group(function () {
     Route::get('/mypage', [ProfileController::class, 'show'])->name('profiles.show');
     Route::get('/mypage/profile', [ProfileController::class, 'edit'])->name('profiles.edit');
