82dd58657ab3e06600daa613689a756a3dcfab60
ai2525desu <umi08062525@gmail.com>
2025-09-08 01:26:20 +0900

    詳細画面レスポンシブ＋レイアウト完成

--- src/resources/views/items/detail.blade.php
+++ src/resources/views/items/detail.blade.php
@@ -11,16 +11,23 @@
     </div>
     <div class="detail-content__product-introduction">
         <h2 class="product-introduction__heading">{{ $item->item_name }}</h2>
-        <p>{{ $item->brand }}</p>
+        <p class="product-introduction__brand">ブランド名：{{ $item->brand }}</p>
+        <div class="product-introduction__price">
+            <span class="product-introduction__price--symbol">¥</span>
+            <span class="product-introduction__price--amount">
+                {{ $item->price }}
+            </span>
+            <span class="product-introduction__price--symbol">(税込)</span>
+        </div>
         <div class="product-introduction__count">
             <!-- ここがいいね機能になるとすれば、formでmethodPOSTかな？ -->
             <div class="product-introduction__count--likes">
                 <img src="{{ asset('storage/star.png') }}" art="いいね">
-                <span>いいねの数</span>
+                <span class="product-introduction__count-number">いいねの数</span>
             </div>
             <div class="product-introduction__count--comments">
                 <img src="{{ asset('storage/speech-bubble.png') }}" art="コメント">
-                <span>コメントの数</span>
+                <span class="product-introduction__count-number">コメントの数</span>
             </div>
         </div>
     </div>
@@ -28,33 +35,41 @@
         <a class="screen-transition__purchase" href="{{ route('purchases.show', ['item_id' => $item->id]) }}">購入手続きへ</a>
     </div>
     <div class="detail-content__description">
-        <h2 class="description__heading">商品説明</h2>
-        <p>{{ $item->description }}</p>
+        <h2 class="detail-content__description-heading">商品説明</h2>
+        <p class="detail-content__description-content">{{ $item->description }}</p>
     </div>
     <div class="detail-content__information">
-        <div class="detail-content__information--category">
-            <p>カテゴリー</p>
-            @foreach ($item->categories as $category)
-            <span>{{ $category->category_name }}</span>
-            @endforeach
+        <h2 class="detail-content__information-heading">
+            商品の情報
+        </h2>
+        <div class="detail-content__information-category">
+            <p class="category__title">カテゴリー</p>
+            <div class="category__item-wrap">
+                @foreach ($item->categories as $category)
+                <span class="category__item">{{ $category->category_name }}</span>
+                @endforeach
+            </div>
         </div>
-        <div class="detail-content__information--condition">
-            <p>商品の状態</p>
-            <span>{{ $condition }}</span>
+        <div class="detail-content__information-condition">
+            <p class="condition__title">商品の状態</p>
+            <div class="condition__item-wrap">
+                <span class="condition__item">{{ $condition }}</span>
+            </div>
         </div>
     </div>
-    <div class="detail-content__information--comment">
-        <h2 class="comment__heading">コメント＋その数のカウント数</h2>
+    <div class="detail-content__comment">
+        <h2 class="comment__heading">コメント+count()</h2>
         <div class="comment__existing-comment">
             ここにすでにコメントされている内容が表示される
             ユーザー画像＋名前
             コメント内容
         </div>
         <div class="comment__new-comment">
+            <!-- action未設定 -->
             <form class="new-comment__form" action="" method="post" enctype="multipart/form-data" novalidate>
                 @csrf
                 <div class="new-comment__form-title">
-                    <label class="new-comment__form--label">商品へのコメント</label>
+                    <label class="new-comment__form-label">商品へのコメント</label>
                 </div>
                 <div class="new-comment__form-content">
                     <textarea name="comment_content">{{ old('comment_content') }}</textarea>
--- src/resources/views/profiles/show.blade.php
+++ src/resources/views/profiles/show.blade.php
@@ -1,60 +1,64 @@
-@extends('layouts.app')
+    font-family: "Inter",
+    sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 700;
+    font-style: normal;@extends('layouts.app')
 
-@section('css')
-<link rel="stylesheet" href="{{ asset('css/profiles/show.css') }}">
-@endsection
+    @section('css')
+    <link rel="stylesheet" href="{{ asset('css/profiles/show.css') }}">
+    @endsection
 
-@section('content')
-<div class="profile-show-content__wrap">
-    <div class="profile-show-content__user-information">
-        <div class="user-information__profile-image">
-            @if ($user->profile?->image)
-            <img src="{{  asset('storage/' . $user->profile->image) }}" class="profile-image">
-            @endif
-        </div>
-        <p class="user-information__user-name">
-            {{ $user->name }}
-        </p>
-        <div class="user-information__screen-transition">
-            <a class="screen-transition__edit" href="{{ route('profiles.edit') }}">プロフィールを編集
-            </a>
-        </div>
-    </div>
-    <div class="profile-show-content__product-information">
-        <div class="product-tab__header">
-            <ul class="product-tab__heading">
-                <li class="product-tab__item">
-                    <a class="item__exhibition-list {{ $tab === 'sell' ? 'is-active' : '' }}" href="{{ route('profiles.show', ['tab' => 'sell']) }}">出品した商品</a>
-                </li>
-                <li class="product-tab__item">
-                    <a class="item__purchase-list {{ $tab === 'buy' ? 'is-active' : '' }}" href="{{ route('profiles.show', ['tab' => 'buy']) }}">購入した商品</a>
-                </li>
-            </ul>
-        </div>
-        <div class="product-tab__body">
-            <div class="product-tab__content {{ $tab === 'sell' ? 'is-active' : '' }}">
-                <div class="exhibition-product-card__list">
-                    @foreach ($user->items as $item)
-                    <div class="exhibition-product-card__wrap">
-                        <img src="{{ asset('storage/' . $item->item_image) }}" alt="{{ $item->item_name }}">
-                        <p class="exhibition-card__title">{{ $item->item_name }}</p>
-                    </div>
-                    @endforeach
-                </div>
+    @section('content')
+    <div class="profile-show-content__wrap">
+        <div class="profile-show-content__user-information">
+            <div class="user-information__profile-image">
+                @if ($user->profile?->image)
+                <img src="{{  asset('storage/' . $user->profile->image) }}" class="profile-image">
+                @endif
             </div>
-            <div class="product-tab__content {{ $tab === 'buy' ? 'is-active' : '' }}">
-                <div class="purchase-product-card__wrap">
-                    @foreach ($user->purchases as $purchase)
-                    @if ($purchase->item)
+            <p class="user-information__user-name">
+                {{ $user->name }}
+            </p>
+            <div class="user-information__screen-transition">
+                <a class="screen-transition__edit" href="{{ route('profiles.edit') }}">プロフィールを編集
+                </a>
+            </div>
+        </div>
+        <div class="profile-show-content__product-information">
+            <div class="product-tab__header">
+                <ul class="product-tab__heading">
+                    <li class="product-tab__item">
+                        <a class="item__exhibition-list {{ $tab === 'sell' ? 'is-active' : '' }}" href="{{ route('profiles.show', ['tab' => 'sell']) }}">出品した商品</a>
+                    </li>
+                    <li class="product-tab__item">
+                        <a class="item__purchase-list {{ $tab === 'buy' ? 'is-active' : '' }}" href="{{ route('profiles.show', ['tab' => 'buy']) }}">購入した商品</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="product-tab__body">
+                <div class="product-tab__content {{ $tab === 'sell' ? 'is-active' : '' }}">
+                    <div class="exhibition-product-card__list">
+                        @foreach ($user->items as $item)
+                        <div class="exhibition-product-card__wrap">
+                            <img src="{{ asset('storage/' . $item->item_image) }}" alt="{{ $item->item_name }}">
+                            <p class="exhibition-card__title">{{ $item->item_name }}</p>
+                        </div>
+                        @endforeach
+                    </div>
+                </div>
+                <div class="product-tab__content {{ $tab === 'buy' ? 'is-active' : '' }}">
                     <div class="purchase-product-card__wrap">
-                        <img src="{{ asset('storage/' . $purchase->item->item_image) }}" alt="{{ $purchase->item->item_name }}">
-                        <p class="purchase-card__title">{{ $purchase->item->item_name }}</p>
+                        @foreach ($user->purchases as $purchase)
+                        @if ($purchase->item)
+                        <div class="purchase-product-card__wrap">
+                            <img src="{{ asset('storage/' . $purchase->item->item_image) }}" alt="{{ $purchase->item->item_name }}">
+                            <p class="purchase-card__title">{{ $purchase->item->item_name }}</p>
+                        </div>
+                        @endif
+                        @endforeach
                     </div>
-                    @endif
-                    @endforeach
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+    @endsection
