35a07a374e63f4d56cf96b40228263668eecf5d2
ai2525desu <umi08062525@gmail.com>
2025-10-09 22:24:12 +0900

    商品検索テスト完了

--- src/tests/Feature/ItemSearchTest.php
+++ src/tests/Feature/ItemSearchTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\Item;
+use App\Models\Like;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -31,6 +33,33 @@ class ItemSearchTest extends TestCase
         $response = $this->get('/?item_name=腕');
         $response->assertStatus(200);
         $response->assertSee('腕時計');
+        $response->assertDontSee('玉ねぎ');
+    }
+
+    // 商品検索がマイリストでも保持されるかのテスト
+    public function test_confirm_retention_of_search_data_in_mylist()
+    {
+        $searchUser = User::factory()->create();
+
+        $response = $this->post('/login', [
+            'email' => $searchUser->email,
+            'password' => 'password',
+        ]);
+        $response->assertRedirect('/');
+        $this->assertAuthenticatedAs($searchUser);
+
+        $likedItem = Item::first();
+
+        Like::create([
+            'user_id' => $searchUser->id,
+            'item_id' => $likedItem->id,
+        ]);
+
+        $response = $this->get('/?item_name=腕');
+        $response->assertStatus(200)->assertSee('腕時計');
+
+        $response = $this->get('/?tab=mylist&item_name=腕');
+        $response->assertStatus(200)->assertSee('腕時計');
         $response->assertDontSee('玉ねぎ');
     }
 }
